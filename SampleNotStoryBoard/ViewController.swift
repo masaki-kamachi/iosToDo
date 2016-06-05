267e7832f23d47051dd9ebf734c70011a7c81eba
masaki-kamachi <masaki.kamachi@bizreach.co.jp>
2016-06-05 16:55:17 +0900

    Initial Commit

---
+++ SampleNotStoryBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleNotStoryBoard
+//
+//  Created by masakikamachi on 2016/06/05.
+//  Copyright © 2016年 masakikamachi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
